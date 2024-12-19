d6ac46a8f64d92ca916bcdbcdaa950aadf19d7f3
Viacheslav417865 <sfinks417865@gmail.com>
2024-12-19 16:40:12 +0200

    Add django-debug-toolbar configuration

--- api_service_theater/settings.py
+++ api_service_theater/settings.py
@@ -26,9 +26,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 # Application definition
 
 INSTALLED_APPS = [
+    "debug_toolbar",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -48,6 +53,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
--- api_service_theater/urls.py
+++ api_service_theater/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,4 +26,4 @@ urlpatterns = [
         include("theatre.urls",
                 namespace="theatre")
     ),
-]
+] + debug_toolbar_urls()
